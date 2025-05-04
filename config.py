44d59d46e4355edc57be8ff6fb45289a0cbd34e9
KrishnaVamsibakka <bakkakrishnavamsireddy09@gmail.com>
2025-05-04 17:51:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "29055333")
 
 API_HASH = os.environ.get("API_HASH", "a6d154242eaef80a163bf5d0a7763882")
 
